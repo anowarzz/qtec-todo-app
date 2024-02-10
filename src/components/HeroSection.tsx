47a7dda3429b7b6bbfa0dfdb81a59979ec5c2683
anowarzz <anowarhosen444@gmail.com>
2024-02-10 18:52:16 +0600

    completed basic design of hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,21 +3,21 @@ import TodoImage from "../assets/task.png";
 
 const HeroSection = () => {
   return (
-    <div className="container mt-14 md:mt-20 px-4 lg:px-8">
-      <div className="flex flex-col md:flex-row justify-center items-center gap-6  ">
-        <div className="">
+    <div className="container mt-8 md:mt-12 px-4 lg:px-8 mx-auto">
+      <div className="flex flex-col md:flex-row justify-end  items-center gap-6  ">
+        <div className="w-full">
           <div className="px-4">
-            <h1 className=" font-semibold  text-[#09060c] text-2xl md:text-3xl lg:text-4xl mb-2">
+            <h1 className=" font-semibold text-center md:text-left text-[#09060c] text-2xl md:text-3xl lg:text-4xl mb-2">
               Turn Chaos into Order
             </h1>
-            <h1 className=" font-semibold  text-[#09060c] text-2xl md:text-3xl lg:text-4xl my-2">
+            <h1 className=" font-semibold text-center md:text-left text-[#09060c] text-2xl md:text-3xl lg:text-4xl my-2">
               with QTEC TODO
             </h1>
           </div>
-          <p className="text-lg my-2 text-gray-500 text-wrap">
-            Your personal task management tool. Organize your tasks, prioritize
-            your work, and track your progress, all in one place. Start
-            achieving more today with @workspace.
+          <p className="text-lg text-gray-500 text-wrap px-4 text-justify mt-4">
+            Your personal task management tool. Organize your tasks{" "}
+            <br className="hidden md:block" /> and Start achieving more today
+            with QTec Todo.
           </p>
         </div>
         <div className=" w-2/3  flex justify-center items-center">
