e571a5d4354380dab33931a73fb8d957fa196c4d
anowarzz <anowarhosen444@gmail.com>
2024-02-10 14:46:02 +0600

    project initialized with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-blue-500 text-center">
+        Hello World , From React App
+      </h1>
+    </div>
+  );
+};
+
+export default App;
